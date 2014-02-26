b70605d3e58ec60f26c5239f408a58db66d0f9a5
unknown <adriancarayol@gmail.com>
2014-02-26 20:27:58 +0100

    change1

--- skyfolk/static/js/profile.js
+++ skyfolk/static/js/profile.js
@@ -1,51 +1,46 @@
+<script> 
 $(document).ready(function(){
-
-  	$("#Menu").mouseover(function(){
-    	$("#panel").slideToggle("slow");
-	});
-
-  	$("#Menu").mouseover(function(){
-	    $("#panel").animate({
-	      left:'250px',
-	      opacity:'0.9',
-	      height:'0px',
-	      width:'0px'
-	    });
-	});
-
-
-				//Examples of how to assign the ColorBox event to elements
-                $(".inline").colorbox({inline:true, width:"78%"});
-				$(".callbacks").colorbox({
-					onOpen:function(){ alert('onOpen: colorbox is about to open'); },
-					onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
-					onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
-					onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
-					onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
-				});
-				
-				//Example of preserving a JavaScript event for inline calls.
-				$("#click").click(function(){ 
-					$('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
-					return false;
-				});
-
-
+  $("#flip").click(function(){
+    $("#panel1").slideToggle("slow");
+  });
+});
+</script>
+<script> 
+$(document).ready(function(){
+  $("#Menu").mouseover(function(){
+    $("#panel").slideToggle("slow");
+	  });
 });
 
+$(document).ready(function(){
+  $("#Menu").mouseover(function(){
+    $("#panel").animate({
+      left:'250px',
+      opacity:'0.9',
+      height:'300px',
+      width:'300px'
+    });
+  });
+});
+</script> 
+<script language="javascript">
+function aparecerbola(){
+	document.getElementById("widget").style.opacity = "0.9";
+	document.getElementById("widget").style.webkitTransition = "opacity 1s linear";
+}
+function cambiopagina(){
+	document.getElementById("widget").style.opacity = "0";
+	document.getElementById("widget").style.webkitTransition = "opacity 0.9s linear";
+	setTimeout('location.href="columnas.php"', 500);
+}
+function cambiopagina(){
+	document.getElementById("widget").style.opacity = "0";
+	document.getElementById("widget").style.webkitTransition = "opacity 0.9s linear";
+	setTimeout('location.href="columnas.php"', 500);
+}
 
 
-	function aparecerbola(){
-		document.getElementById("widget").style.opacity = "0.9";
-		document.getElementById("widget").style.webkitTransition = "opacity 1s linear";
-	}
-	function cambiopagina(){
-		document.getElementById("widget").style.opacity = "0";
-		document.getElementById("widget").style.webkitTransition = "opacity 0.9s linear";
-		setTimeout('location.href="columnas.php"', 500);
-	}
-	function cambiopagina(){
-		document.getElementById("widget").style.opacity = "0";
-		document.getElementById("widget").style.webkitTransition = "opacity 0.9s linear";
-		setTimeout('location.href="columnas.php"', 500);
-	}
+
+
+</script>
+
