74569260affc86a45c4feef99e00441aae8b996c
unknown <adriancarayol@gmail.com>
2014-02-27 19:41:36 +0100

    cambiosa

--- skyfolk/static/js/profile.js
+++ skyfolk/static/js/profile.js
@@ -1,11 +1,10 @@
-<script> 
+
 $(document).ready(function(){
   $("#flip").click(function(){
     $("#panel1").slideToggle("slow");
   });
 });
-</script>
-<script> 
+
 $(document).ready(function(){
   $("#Menu").mouseover(function(){
     $("#panel").slideToggle("slow");
@@ -22,8 +21,8 @@ $(document).ready(function(){
     });
   });
 });
-</script> 
-<script language="javascript">
+
+
 function aparecerbola(){
 	document.getElementById("widget").style.opacity = "0.9";
 	document.getElementById("widget").style.webkitTransition = "opacity 1s linear";
@@ -42,5 +41,24 @@ function cambiopagina(){
 
 
 
-</script>
 
+
+
+			$(document).ready(function(){
+				//Examples of how to assign the ColorBox event to elements
+                $(".inline").colorbox({inline:true, width:"78%"});
+				$(".callbacks").colorbox({
+					onOpen:function(){ alert('onOpen: colorbox is about to open'); },
+					onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
+					onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
+					onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
+					onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
+				});
+				
+				//Example of preserving a JavaScript event for inline calls.
+				$("#click").click(function(){ 
+					$('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
+					return false;
+				});
+			});
+		
