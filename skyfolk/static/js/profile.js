ef262fa3984a66a3ea7ef66eb2f84cc5fee9ea18
adrian <adriancarayol@gmail.com>
2014-06-13 20:23:45 +0200

    jq

--- skyfolk/static/js/profile.js
+++ skyfolk/static/js/profile.js
@@ -68,18 +68,27 @@ function cambiopagina(){
 			
 			
 			
-<script type="text/javascript">
-    $(document).ready(function()
-        {
-        $(".nameact").click(function () {     
-            $('#panel1').toggle("slow");
-            });
-        $(".nameact").click(function () {     
-            $('#panel1').toggle("fast");
-            });
-        $(".nameact").click(function () {     
-            $('#panel1').toggle(5000);
-            });
-         });
-</script>
+$(document).ready(function(){
+  $(".nameact").click(function(){
+    $("#panel1").slideToggle("slow");
+	  });
+});
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel1").slideToggle("slow");
+  });
+});
+
+
+
+$(document).ready(function(){
+  $(".nameact").click(function(){
+    $("#panel1").animate({
+      left:'250px',
+      opacity:'1',
+      height:'150px',
+      width:'150px'
+    });
+  });
+});
 
