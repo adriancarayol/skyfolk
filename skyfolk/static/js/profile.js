f17328872c707a01cd347c769672c6e4fb4acfe1
unknown <adriancarayol@gmail.com>
2014-06-14 15:36:39 +0200

    flot

--- skyfolk/static/js/profile.js
+++ skyfolk/static/js/profile.js
@@ -97,3 +97,19 @@ $(document).ready(function(){
   });
 
 /* Mensaje flotante */
+$(document).ready(function(){
+   $(".entypo-mail").click(function () {
+      $("#page-wrapper").each(function() {
+        displaying = $(this).css("display");
+        if(displaying == "block") {
+          $(this).fadeOut('slow',function() {
+           $(this).css("display","none");
+          });
+        } else {
+          $(this).fadeIn('slow',function() {
+            $(this).css("display","block");
+          });
+        }
+      });
+    });
+  });
