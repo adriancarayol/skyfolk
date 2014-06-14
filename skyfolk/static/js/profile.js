48c069e264bcd4bcf7808cc220b577961cc1773b
unknown <adriancarayol@gmail.com>
2014-06-14 15:59:07 +0200

    erer

--- skyfolk/static/js/profile.js
+++ skyfolk/static/js/profile.js
@@ -102,11 +102,11 @@ $(document).ready(function(){
       $("#page-wrapper").each(function() {
         displaying = $(this).css("display");
         if(displaying == "block") {
-          $(this).fadeIn('slow',function() {
+          $(this).fadeOut('slow',function() {
            $(this).css("display","none");
           });
         } else {
-          $(this).fadeOut('slow',function() {
+          $(this).fadeIn('slow',function() {
             $(this).css("display","block");
           });
         }
