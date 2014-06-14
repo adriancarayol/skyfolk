84a980b7e9df8b46dd29c40109281082ec8262f9
Carlos <canicio7@gmail.com>
2014-06-14 18:58:36 +0200

    nuevos modelos

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -46,6 +46,7 @@ class SignupForm(forms.Form):
     def save(self, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
+        user.profile.image = None
         user.save()
 
 
