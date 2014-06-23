b781bce20222a9fd2b6e86eb3dad0f2c4b834044
unknown <adriancarayol@gmail.com>
2014-06-23 13:24:18 +0200

    js

--- skyfolk/static/js/notia.js
+++ skyfolk/static/js/notia.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".fontawesome-bell-alt").click(function () {
+  $(".notificationicon").click(function () {
     $(this).toggleClass("open");
     $("#notificationMenu").toggleClass("open");
   });
