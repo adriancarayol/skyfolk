a9af143005c7dc3239c7884716b18ba7c03d052d
adrian <adriancarayol@gmail.com>
2014-08-03 18:22:47 +0200

    js

--- skyfolk/static/js/profile.js
+++ skyfolk/static/js/profile.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
    $("#Menu").hover(function () {
       $("#panel").each(function() {
-        displaying = $(this).delay(400).css("display");
+        displaying = $(this).delay(200).css("display");
         if(displaying == "block") {
-          $(this).delay(400).fadeOut('slow',function() {
-           $(this).delay(400).css("display","none");
+          $(this).delay(200).fadeOut('slow',function() {
+           $(this).delay(200).css("display","none");
           });
         } else {
-          $(this).delay(400).fadeIn('slow',function() {
-            $(this).delay(400).css("display","block");
+          $(this).delay(200).fadeIn('slow',function() {
+            $(this).delay(200).css("display","block");
           });
         }
       });
