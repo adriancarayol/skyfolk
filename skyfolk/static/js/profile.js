dc0e46c9d9143f39898e7bc0d6a2914082d0c695
unknown <adriancarayol@gmail.com>
2014-08-10 19:53:44 +0200

    fkcrew

--- skyfolk/static/js/profile.js
+++ skyfolk/static/js/profile.js
@@ -243,3 +243,20 @@ $(document).ready(function(){
       });
     });
   });
+
+$(document).ready(function(){
+   $(".fontawesome-remove").click(function () {
+      $("#fastconf").each(function() {
+        displaying = $(this).css("display");
+        if(displaying == "block") {
+          $(this).fadeOut('slow',function() {
+           $(this).css("display","none");
+          });
+        } else {
+          $(this).fadeIn('slow',function() {
+            $(this).css("display","none");
+          });
+        }
+      });
+    });
+  });
