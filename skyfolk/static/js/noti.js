ac642d2b483fb83112c358a10427825604255121
adrian <adriancarayol@gmail.com>
2014-09-04 18:55:09 +0200

    l

--- skyfolk/static/js/noti.js
+++ skyfolk/static/js/noti.js
@@ -11,19 +11,3 @@ $(document).ready(function() {
     closeMessage($('#js-timer'));
   }, 5000);
 });
-
-
-function closeMessage(el) {
-  el.addClass('is-hidden');
-}
-
-$('a[target="x"]').on('click', function(e) {
-  closeMessage($(this).closest('.love'));
-});
-
-$(document).ready(function() {
-  setTimeout(function() {
-    closeMessage($('.love'));
-  }, 5000);
-});
-
