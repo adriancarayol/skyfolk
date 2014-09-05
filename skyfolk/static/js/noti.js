2290176518e09af571745dcf0316469ec1796616
unknown <adriancarayol@gmail.com>
2014-09-05 15:19:08 +0200

    l

--- skyfolk/static/js/noti.js
+++ skyfolk/static/js/noti.js
@@ -1,3 +1,4 @@
+/* NOTIFICACION BIENVENIDA */
 function closeMessage(el) {
   el.addClass('is-hidden');
 }
@@ -9,5 +10,20 @@ $('.js-messageClose').on('click', function(e) {
 $(document).ready(function() {
   setTimeout(function() {
     closeMessage($('#js-timer'));
+  }, 3000);
+});
+
+/* NOTIFICACION ME GUSTA */
+function closeMessage(el) {
+  el.addClass('is-hidden');
+}
+
+$('.js-messageClose').on('click', function(e) {
+  closeMessage($(this).closest('.Message'));
+});
+
+$(document).ready(function() {
+  setTimeout(function() {
+    closeMessage($('#.love'));
   }, 5000);
 });
