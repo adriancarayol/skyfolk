b51ee0a218026ad5b10fbe54deb5c4d17fa9e35b
adrian <adriancarayol@gmail.com>
2014-09-10 13:48:49 +0200

    lluvia

--- skyfolk/static/js/amigos.js
+++ skyfolk/static/js/amigos.js
@@ -1,10 +1,30 @@
+
+
+/* abrir */
 $(document).ready(function(){
-   $(".fontawesome-remove").click(function () {
+   $(".fontawesome-plus").click(function () {
       $("#section").each(function() {
         displaying = $(this).css("display");
         if(displaying == "none") {
           $(this).fadeOut('slow',function() {
            $(this).css("display","block");
+          });
+        } else {
+          $(this).fadeIn('slow',function() {
+            $(this).css("display","none");
+          });
+        }
+      });
+    });
+  });
+/* cerrar */ 
+$(document).ready(function(){
+   $(".fontawesome-remove").click(function () {
+      $("#sectiom").each(function() {
+        displaying = $(this).css("display");
+        if(displaying == "block") {
+          $(this).fadeOut('slow',function() {
+           $(this).css("display","none");
           });
         } else {
           $(this).fadeIn('slow',function() {
