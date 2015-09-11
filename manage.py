7a63f469556a253005daae01b1af91d0c4260c7d
lostcitizen <lostcitizen@gmail.com>
2015-09-11 05:34:45 +0200

    fix manage.py automatico

--- manage.py
+++ manage.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
-import os
-import sys
+
+import subprocess
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skyfolk.settings.pre")
+    # Verficamos con git en que rama nos encontramos
+    stdoutdata = subprocess.check_output("git status", shell=True)
 
-    from django.core.management import execute_from_command_line
+    # Compara byte array
+    if b'On branch develop' in stdoutdata:
+        import manage.manage_develop
+        #exec(open('manage/manage_develop.py').read())
+    elif b'On branch pre' in stdoutdata:
+        import manage.manage_pre
+        #exec(open('manage/manage_pre.py').read())
+    elif b'On branch master' in stdoutdata:
+        import manage.manage_master
+        #exec(open('manage/manage_master.py').read())
+    else:
+        pass
 
-    execute_from_command_line(sys.argv)
