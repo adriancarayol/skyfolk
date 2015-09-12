149dd2a86d524e3288967dfcefbe4c03dfd174b4
lostcitizen <lostcitizen@gmail.com>
2015-09-12 05:25:54 +0200

    manage.py wrapper Acepta parametro --entorno y selecciona el manage.py adecuado en el paquete manage/
    Por defecto (sin parametro) carga el entorno de desarrollo.

--- manage.py
+++ manage.py
@@ -4,19 +4,35 @@ import os
 import sys
 
 if __name__ == "__main__":
+    deploy = False
+    try:
+        primerParam = str(sys.argv[1])
+        segundoParam = str(sys.argv[2])
+    except:
+        print("catch")
+        primerParam = None
+        segundoParam = None
 
-
-    if os.path.isfile("/var/www/skyfolk/run/deploy-master.lock"):
-        entorno = "master"
-    elif os.path.isfile("/var/www/skyfolk/run/deploy-pre.lock"):
-        entorno = "pre"
+    print("primerParam: " + primerParam + " / segundoParam" + segundoParam)
+    if primerParam == "--entorno" and ( segundoParam == "develop" or
+                                        segundoParam == "pre" or
+                                        segundoParam == "master" ):
+        print("Antes de extraer parametros: " + str(sys.argv))
+        deploy = True
+        entorno = segundoParam
+        sys.argv.remove(str(sys.argv[1]))
+        sys.argv.remove(str(sys.argv[1]))
+        print("Despues de extraer parametros: " + str(sys.argv))
     else:
+        #develop
         entorno = "develop"
 
+    print("entorno: " + entorno + " / argv: " + str(sys.argv))
     if entorno == "master":
         import manage.manage_master
     elif entorno == "pre":
         import manage.manage_pre
     else:
+        #develop
         import manage.manage_develop
 
