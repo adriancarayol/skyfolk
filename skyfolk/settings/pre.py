7d8ed8113fcdec34fd4f3180e5f65e758bdb6932
lostcitizen <lostcitizen@gmail.com>
2015-09-17 03:41:32 +0200

    fix pre-master-develop mail settings

--- skyfolk/settings/pre.py
+++ skyfolk/settings/pre.py
@@ -12,7 +12,7 @@ DEBUG = True
 #
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'dfgsdfgsdf906@gmail.com'
 EMAIL_HOST_PASSWORD = '56g4eD&%&FGfgdsf'
 
