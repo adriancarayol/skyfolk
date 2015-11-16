506d6afc0bbf0b460f4351d662bbe2b78930214e
lostcitizen <lostcitizen@gmail.com>
2015-11-16 11:49:11 +0100

    fix migraciones

---
+++ publications/migrations/0017_auto_20151116_1135.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('publications', '0016_auto_20151116_1024'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='publication',
+            name='likes',
+            field=models.IntegerField(default=0, null=True, blank=True),
+        ),
+    ]
