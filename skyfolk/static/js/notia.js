a9beb3d8275072192834ffd7a600a92acfa7b693
adrian <adriancarayol@gmail.com>
2016-01-06 00:56:06 +0100

    fix

--- skyfolk/static/js/notia.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $(".notifications .messages").hide();
-  $(".notifications").click(function() {
-    if ($(this).children(".messages").children().length > 0) {
-      $(this).children(".messages").fadeToggle(300);
-    }
-  });
-});
