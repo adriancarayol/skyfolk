b020e1d5f2faeefd706a0881ddcd9cde51302176
adrian <adriancarayol@gmail.com>
2016-01-28 01:13:32 +0100

    + Version V1 + Eliminando recursos externos

--- skyfolk/static/js/profile.js
+++ skyfolk/static/js/profile.js
@@ -987,9 +987,6 @@ $(document).ready(function() {
   });
 });
 
- $(document).ready(function() {
-    $("#page-wrapper").draggable();
-  });
 function addItemToFriendList(name, lastname) {
 
   $("#tab-amigos ul").append('<li><img src="{{STATIC_URL}}img/generic-avatar.png" class="img-responsive"><a>' + name + ' ' + lastname + '</a></li>');
