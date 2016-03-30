57f2fa8ea9dd50ade072ed1db91c3ca7f3dcb6d5
acarayol <adriancarayol@gmail.com>
2016-03-30 23:24:02 +0200

    + Version V1 + Comentarios con negrita y cursiva

--- publications/templatetags/tags_mentions.py
+++ publications/templatetags/tags_mentions.py
@@ -5,8 +5,15 @@ register = template.Library()
 
 @register.filter(name='replace_tags_mentions')
 def replace_tags(value):
-    hashtags = re.findall('#[a-zA-Z][a-zA-Z0-9_]*', value)
+    bold = re.findall('\*[^\*]+\*', value)
+    ''' Bold para comentario '''
+    for b in bold:
+        value = value.replace(b, '<b>%s</b>' % (b[1:len(b)-1]))
+    italic = re.findall('_[^_]+_', value)
+    for i in italic:
+        value = value.replace(i, '<i>%s</i>' % (i[1:len(i) - 1]))
     ''' Tags para comentario '''
+    hashtags = re.findall('#[a-zA-Z][a-zA-Z0-9_]*', value)
     for hashtag in hashtags:
         value = value.replace(hashtag, '<a href="/search/">%s</a>' % (hashtag))
     ''' Menciones para comentario '''
