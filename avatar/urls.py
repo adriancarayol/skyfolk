a9715a8891a429c3f917e8b17c9e378acabc41de
acarayol <adriancarayol@gmail.com>
2016-04-17 18:24:34 +0200

    + Interfaz django-avatar

--- avatar/urls.py
+++ avatar/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from avatar import views
 
 urlpatterns = [
-    url(r'^config/profile/add/avatar/$', views.add, name='avatar_add'),
-    url(r'^config/profile/change/avatar/$', views.change, name='avatar_change'),
-    url(r'^config/profile/delete/avatar/$', views.delete, name='avatar_delete'),
+    url(r'^/profile/add/avatar/$', views.add, name='avatar_add'),
+    url(r'^/profile/change/avatar/$', views.change, name='avatar_change'),
+    url(r'^/profile/delete/avatar/$', views.delete, name='avatar_delete'),
     url(r'^render_primary/(?P<user>[\w\d\@\.\-_]+)/(?P<size>[\d]+)/$',
         views.render_primary,
         name='avatar_render_primary'),
