6438f22dada289e819ebeabcc6884aa0bcf60b1f
adrian <adriancarayol@gmail.com>
2016-07-22 14:04:12 +0200

    + V2 + Timeline (Comentarios 95%)

--- text_processor/templatetags/decorators.py
+++ text_processor/templatetags/decorators.py
@@ -1,8 +1,9 @@
 from django import template
+from publications.models import Publication
 import re
 register = template.Library()
 
-
+# Remplaza ocurrencias en el contenido del comentario
 @register.filter(name='replace_decorators')
 def replace_tags(value):
     bold = re.findall('\*[^\*]+\*', value)
@@ -18,3 +19,12 @@ def replace_tags(value):
     for i in tachado:
         value = value.replace(i, '<strike>%s</strike>' % (i[1:len(i) - 1]))
     return value
+
+# Devuelve el numero total de veces que se ha compartido un comentario
+@register.filter(name='total_shares')
+def total_shares(pub):
+    total = Publication.objects.get(pk=pub).user_share_me.count()
+    if total > 0:
+        return total
+    else:
+        return ""
