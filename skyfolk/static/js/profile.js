b0e44e213d4a28e20ffc401d4a38ec4fc72364eb
adrian <adriancarayol@gmail.com>
2016-08-11 19:30:19 +0200

    + V3 + sass + para galería de imágenes

--- skyfolk/static/js/profile.js
+++ skyfolk/static/js/profile.js
@@ -10,39 +10,10 @@ var flag_reply = false;
 
 $(document).ready(function () {
 
-    //Examples of how to assign the ColorBox event to elements (Galeria)
-    $(".inline").colorbox({
-        inline: true,
-        width: "80%"
-      });
-    $(".callbacks").colorbox({
-        onOpen: function() {
-          alert('onOpen: colorbox is about to open');
-        },
-        onLoad: function() {
-          alert('onLoad: colorbox has started to load the targeted content');
-        },
-        onComplete: function() {
-          alert('onComplete: colorbox has displayed the loaded content');
-        },
-        onCleanup: function() {
-          alert('onCleanup: colorbox has begun the close process');
-        },
-        onClosed: function() {
-          alert('onClosed: colorbox has completely closed');
-        }
-      });
-
-      //Example of preserving a JavaScript event for inline calls.
-      $("#click").click(function() {
-        $('#click').css({
-          "background-color": "#f00",
-          "color": "#fff",
-          "cursor": "inherit"
-        }).text("Open this window again and this message will still be here.");
-        return false;
-      });
-
+    /* Display gallery */
+    $('.to_gallery').on('click', function () {
+        $("#gallery").toggle();
+    });
     /* Show more - Show less */
     $('#tab-comentarios').find('.wrapper').each(function () {
         var showLimitChar = 90;
