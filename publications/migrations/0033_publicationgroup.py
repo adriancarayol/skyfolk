fde8af3e0c06dbc73231a6563d1634364d7b572c
adrian <adriancarayol@gmail.com>
2016-12-19 01:46:26 +0100

    + V4 + Model para publicaciones en grupos

---
+++ publications/migrations/0033_publicationgroup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('user_groups', '0015_remove_usergroups_likes'),
+        ('publications', '0032_auto_20160926_2300'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='PublicationGroup',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('board_group', models.ForeignKey(related_name='board_group', to='user_groups.UserGroups')),
+                ('parent', models.ForeignKey(related_name='reply_group', blank=True, null=True, to='publications.PublicationGroup')),
+                ('user_give_me_hate', models.ManyToManyField(related_name='hates_group_me', blank=True, to=settings.AUTH_USER_MODEL)),
+                ('user_give_me_like', models.ManyToManyField(related_name='likes_group_me', blank=True, to=settings.AUTH_USER_MODEL)),
+                ('user_share_me', models.ManyToManyField(related_name='share_group_me', blank=True, to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
