08549d878081a5c992ff0ba650b712feb554b945
LostCitizen <lostcitizen@gmail.com>
2017-03-09 19:21:43 +0100

    + Moved 127.0.0.1 in ALLOWED_HOSTS to skyfolk/settings/develop.py + Added localhost to ALLOWED_HOSTS in skyfolk/settings/develop.py

--- skyfolk/settings/base.py
+++ skyfolk/settings/base.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
 DEBUG = False
-ALLOWED_HOSTS = ['.skyfolk.net', '127.0.0.1']
+ALLOWED_HOSTS = ['.skyfolk.net']
 # Allowed html content.
 ALLOWED_TAGS = "p div br code pre h1 h2 h3 h4 hr span s sub sup b i img strong strike em underline super table thead tr th td tbody".split()
 ALLOWED_STYLES = 'color font-weight background-color width height'.split()
