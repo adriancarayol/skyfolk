304d24edac08003906229f9d95e0e96dc1cbacd9
LostCitizen <lostcitizen@gmail.com>
2017-03-12 00:10:47 +0100

    Updated skyfolk/celery.py

--- skyfolk/celery.py
+++ skyfolk/celery.py
@@ -3,8 +3,21 @@ import os
 from celery import Celery
 
 from django.conf import settings
+
+# Cargamos SECRET_KEY
+def get_env_variable(var_name):
+    '''Intenta leer una variable de entorno'''
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+
+DJANGO_SETTINGS_MODULE = get_env_variable('DJANGO_SETTINGS_MODULE')
+
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skyfolk.settings.develop')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skyfolk.settings.develop')
 
 app = Celery('skyfolk')
 
