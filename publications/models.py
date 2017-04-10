5af9ff9dc969d0440dcbf5c2b375804e6e3998b4
adrian <adriancarayol@gmail.com>
2017-04-10 21:43:59 +0200

    + V6 + Mejorando preview de urls en pubs

--- publications/models.py
+++ publications/models.py
@@ -267,6 +267,8 @@ class Publication(PublicationBase):
             r'(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:/\S*)?',
             self.content)
         if link_url and len(link_url) > 0:
+            for u in list(set(link_url)): # Convertimos URL a hipervinculo
+                self.content = self.content.replace(u, '<a href="%s">%s</a>' % (u, u))
 
             url = link_url[-1]  # Get last url
             response = requests.get(url)
@@ -282,16 +284,13 @@ class Publication(PublicationBase):
             self.event_type = 3
             extra_content = ExtraContent.objects.create(url=url)
             if description:
-                extra_content.description = description.get('content', None)
+                extra_content.description = description.get('content', None)[:265]
             if title:
-                extra_content.title = title.get('content', None)
+                extra_content.title = title.get('content', None)[:63]
             if image:
                 extra_content.image = image.get('content', None)
             extra_content.save()
             self.extra_content = extra_content
-
-
-
 
         bold = re.findall('\*[^\*]+\*', self.content)
         bold = list(set(bold))
