074e3fd97a00c871eedaafec7167821928e71c8b
Adrian Carayol <adriancarayol@gmail.com>
2017-10-21 00:47:11 +0200

    + Daphne + runworker

--- run_web.sh
+++ run_web.sh
@@ -13,7 +13,6 @@ su -m skyfolk -c "python manage.py badgify_sync badges"
 su -m skyfolk -c "python manage.py badgify_sync badges --update"
 sleep 10
 su -m skyfolk -c "python manage.py rebuild_index --noinput"
-su -m skyfolk -c "python manage.py runserver 0.0.0.0:8000"
-# su -m skyfolk -c "python manage.py runworker -v2"
-# su -m skyfolk -c "daphne -b 0.0.0.0 -p 8000 skyfolk.asgi:channel_layer"
+# su -m skyfolk -c "python manage.py runserver 0.0.0.0:8000"
+su -m skyfolk -c "daphne -b 0.0.0.0 -p 8000 skyfolk.asgi:channel_layer"
 
---
+++ run_worker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+su -m skyfolk -c "python manage.py runworker -v2"
