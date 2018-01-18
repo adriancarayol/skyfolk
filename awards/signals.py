ea94dd363ec9879f5162c48c5acc7dbf4c48158f
adrian <adriancarayol@gmail.com>
2018-01-18 16:08:45 +0100

    Logros con nuevas imagenes Notificacion al recibir un nuevo logro

--- awards/signals.py
+++ awards/signals.py
@@ -3,6 +3,7 @@ import logging
 from badgify.models import Award
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from notifications.signals import notify
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -10,4 +11,8 @@ logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=Award, dispatch_uid='award_save')
 def awards_handler(sender, instance, created, **kwargs):
-    logger.info('User: {} ha logrado un nuevo logro: {}'.format(instance.user, instance.badge))
+	notify.send(instance.user, actor=instance.user.username,
+		recipient=instance.user,
+		verb=u'¡Nuevo logro conseguido!',
+		description=u'Has obtenido el logro: {}'.format(instance.badge.name))
+	logger.info('User: {} ha logrado un nuevo logro: {}'.format(instance.user, instance.badge))
