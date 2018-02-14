69abdf300098647973dc416de141bd687e9e5688
adrian <adriancarayol@gmail.com>
2018-02-14 18:56:10 +0100

    + RUN WEB FIXED

--- run_web.sh
+++ run_web.sh
@@ -14,7 +14,7 @@ su -m skyfolk -c "python manage.py badgify_sync badges"
 su -m skyfolk -c "python manage.py badgify_sync badges --update"
 sleep 10
 su -m skyfolk -c "python manage.py rebuild_index --noinput"
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('adrian', 'adriancarayol@gmail.com', 'un!x322@')" | python manage.py shell
+# echo "from django.contrib.auth.models import User; User.objects.create_superuser('adrian', 'adriancarayol@gmail.com', 'un!x322@')" | python manage.py shell
 # su -m skyfolk -c "python manage.py runserver 0.0.0.0:8000"
 su -m skyfolk -c "daphne -b 0.0.0.0 -p 8000 skyfolk.asgi:channel_layer"
 
