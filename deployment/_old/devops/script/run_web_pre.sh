cd41d3ae8bb7fa24ff862799857fcafd912ef590
LostCitizen <lostcitizen@gmail.com>
2018-07-27 22:30:33 +0200

    Refactor

--- deployment/_old/devops/script/run_web_pre.sh
+++ deployment/_old/devops/script/run_web_pre.sh
@@ -17,15 +17,16 @@ done
 >&2 echo "neo4j is up - executing command"
 
 # su -m skyfolk -c "command"
-python manage.py makemigrations
-python manage.py makemigrations badgify
-python manage.py migrate badgify
-python manage.py migrate
-python manage.py badgify_sync badges
-python manage.py badgify_sync badges --update
-python manage.py install_labels
-python manage.py rebuild_index --noinput
+python manage.py --entorno pre collectstatic --noinput
+python manage.py --entorno pre makemigrations
+python manage.py --entorno pre makemigrations badgify
+python manage.py --entorno pre migrate badgify
+python manage.py --entorno pre migrate
+python manage.py --entorno pre badgify_sync badges
+python manage.py --entorno pre badgify_sync badges --update
+python manage.py --entorno pre install_labels
+python manage.py --entorno pre rebuild_index --noinput
 # echo "from django.contrib.auth.models import User; User.objects.create_superuser('adrian', 'adriancarayol@gmail.com', 'un!x322@')" | python manage.py shell
 # su -m skyfolk -c "python manage.py runserver 0.0.0.0:8000"
-daphne -b 0.0.0.0 -p 8090 skyfolk.asgi:channel_layer
+daphne -b 0.0.0.0 -p 8000 skyfolk.asgi:channel_layer
 
