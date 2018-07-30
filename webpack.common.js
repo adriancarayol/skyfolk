2dca5d8c9482287ec3ef5b0c68bcdf85694247ce
Adrian Carayol <adriancarayol@gmail.com>
2018-07-30 21:14:11 +0200

    + Add bundles

--- webpack.common.js
+++ webpack.common.js
@@ -7,8 +7,60 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = [
+    {
+        context: __dirname,
+        name: 'react',
+        entry: toObject(glob.sync('./skyfolk-react/src/pages/**/*.js*')),
+        output: {
+            //where you want your compiled bundle to be stored
+            path: path.resolve('./skyfolk/static/js/bundles/'),
+            //naming convention webpack should use for your files
+            filename: '[name]-[hash].js',
+        },
+        plugins: [
+            //tells webpack where to store data about your bundles.
+            new BundleTracker({
+                filename: './webpack-stats.json'
+            }),
+            //makes jQuery available in every module
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery'
+            }),
+            new UglifyJSPlugin({})
+        ],
 
+        module: {
+            rules: [
+                //a regexp that tells webpack use the following loaders on all
+                //.js and .jsx files
+                {
+                    test: /\.jsx?$/,
+                    //we definitely don't want babel to transpile all the files in
+                    //node_modules. That would take a long time.
+                    exclude: /node_modules/,
+                    //use the babel loader
+                    loader: 'babel-loader',
+                    query: {
+                        //specify that we will be dealing with React code
+                        "presets": ['react']
+                    },
+                    enforce: 'pre'
+                },
+            ]
+        },
+        resolve: {
+            //tells webpack where to look for modules
+            modules: ['node_modules'],
+            //extensions that should be used to resolve modules
+            extensions: ['.js', '.jsx']
+        }
+    },
+    {
 
+    }
+    /*
     {
         context: __dirname,
         name: 'css',
@@ -47,7 +99,7 @@ module.exports = [
         new ExtractTextPlugin("bundle.min.css"),
       ]
     }
-
+    */
 ];
 
 function toObject(paths) {
