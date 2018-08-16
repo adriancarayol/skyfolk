2878fce5f1be5ffe21510c7d06510ca611c16956
Adrian Carayol Orenes <adriancarayol@gmail.com>
2018-08-16 20:37:01 +0200

    DONE

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Skyfolk - Social network project",
     author="Adrian Carayol",
     author_email="adriancarayol@gmail.com",
-    url="https://skyfolk.jfrog.io/skyfolk/api/pypi/pypi",
+    url="http://localhost:8081/repository/pypi-internal/",
     version="1",
     install_requires=["nose",],
     packages=["about","account", "publications"],
