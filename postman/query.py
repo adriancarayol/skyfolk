1f6bc9859bcd13f026be77ebd67f6bf5e2376504
Adrian Carayol <adriancarayol@gmail.com>
2018-09-21 21:48:30 +0200

    Fix postman

--- postman/query.py
+++ postman/query.py
@@ -1,5 +1,4 @@
 from __future__ import unicode_literals
-
 from types import MethodType
 
 from django import VERSION
@@ -52,7 +51,7 @@ class CompilerProxy(Proxy, SQLCompiler):
         # mimics compiler.py/SQLCompiler/get_from_clause() and as_sql()
         qn = self.quote_name_unless_alias
         qn2 = self.connection.ops.quote_name
-        alias = self.query.tables[0]
+        alias = self.query.tables[0] if VERSION < (2, 0) else self.query.base_table
         if VERSION >= (1, 8):
             from_clause = self.query.alias_map[alias]
             alias = from_clause.table_alias
@@ -68,10 +67,12 @@ class CompilerProxy(Proxy, SQLCompiler):
             inner_join = self.query.INNER
         index = sql.index(clause) + len(clause)
         extra_table, extra_params = self.union(self.query.pm_get_extra())
+        opts = self.query.get_meta()
+        qn2_pk_col = qn2(opts.pk.column)  # usually 'id' but not in case of model inheritance
         new_sql = [
             sql[:index],
             ' {0} ({1}) {2} ON ({3}.{4} = {2}.{5})'.format(
-                inner_join, extra_table, self.query.pm_alias_prefix, qn(alias), qn2('id'), qn2('id')),
+                inner_join, extra_table, self.query.pm_alias_prefix, qn(alias), qn2_pk_col, qn2_pk_col),
         ]
         if index < len(sql):
             new_sql.append(sql[index:])
@@ -83,12 +84,23 @@ class CompilerProxy(Proxy, SQLCompiler):
         """
         Join several querysets by a UNION clause. Returns the SQL string and the list of parameters.
         """
-        result_sql, result_params = [], []
-        for qs in querysets:
-            sql, params = qs.query.sql_with_params()
-            result_sql.append(sql)
-            result_params.extend(params)
-        return ' UNION '.join(result_sql), tuple(result_params)
+        # union() is "New in Django 1.11." (docs site)
+        # but buggy in 2.0, with a backport in 1.11.8 ; my ticket 29229, fixed in 1.11.12 & 2.0.4.
+        # For simplicity, let's even ignore the usable 1.11.0-7 frame.
+        # Ticket 29286 reintroduced a bug in 1.11.13 & 2.0.5, by considering oly the annotate() case and not the extra().
+        # Ticket 29694 fixed the missing extra() case, but is only effective as of 2.1.1,
+        # because extra() is destined to be deprecated.
+        # So the final solution here was to replace all extra() by annotate() in this app.
+        if VERSION < (1, 11, 12) or (2, 0) <= VERSION < (2, 0, 4):
+            result_sql, result_params = [], []
+            for qs in querysets:
+                sql, params = qs.query.sql_with_params()
+                result_sql.append(sql)
+                result_params.extend(params)
+            return ' UNION '.join(result_sql), tuple(result_params)
+        else:
+            qs = querysets[0].union(*querysets[1:])
+            return qs.query.sql_with_params()
 
 
 class PostmanQuery(Query):
--- postman/views.py
+++ postman/views.py
@@ -257,7 +257,7 @@ class WriteView(ComposeMixin, FormView):
     def get_form_kwargs(self):
         kwargs = super(WriteView, self).get_form_kwargs()
         if isinstance(self.autocomplete_channels, tuple) and len(self.autocomplete_channels) == 2:
-            channel = self.autocomplete_channels[self.request.user.is_anonymous()]
+            channel = self.autocomplete_channels[self.request.user.is_anonymous]
         else:
             channel = self.autocomplete_channels
         kwargs['channel'] = channel
