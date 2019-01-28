cdce2070260c392286c4cf0546f51663286dab8e
Adrian Carayol <adriancarayol@gmail.com>
2019-01-28 22:43:47 +0100

    Fix some bugs

--- user_profile/tasks.py
+++ user_profile/tasks.py
@@ -3,6 +3,8 @@ from mailer.mailer import Mailer
 from skyfolk.celery import app
 from user_profile.models import Profile
 from django.db.models import Count, Q
+from user_profile.models import RelationShipProfile, FOLLOWING
+
 
 logger = get_task_logger(__name__)
 
@@ -15,29 +17,45 @@ def send_email(subject, recipient_list, context, html):
     """
 
     mail = Mailer()
-    mail.send_messages(subject, template=html, context=context, to_emails=recipient_list)
+    mail.send_messages(
+        subject, template=html, context=context, to_emails=recipient_list
+    )
 
 
-@app.task(name='tasks.send_recommendation_via_email')
+@app.task(name="tasks.send_recommendation_via_email")
 def send_recommendation_via_email():
     """
     Envia recomendaciones de usuarios a los usuarios
     registrados en la web
     """
-    nodes = Profile.objects.filter(user__notification_settings__email_when_recommendations=True).order_by('?')
+    nodes = Profile.objects.filter(
+        user__notification_settings__email_when_recommendations=True
+    ).order_by("?")
 
     for profile in nodes:
-        users = Profile.objects.filter(
-            ~Q(user=profile.user)
-            & Q(tags__in=profile.tags.all())
-            & ~Q(privacity="N")
-            & Q(user__is_active=True)
-        ).annotate(similarity=Count("tags")).order_by("-similarity").select_related('user')
+        follows = RelationShipProfile.objects.filter(
+            from_profile=profile, type=FOLLOWING
+        ).values_list("to_profile__id", flat=True)
+        users = (
+            Profile.objects.filter(
+                ~Q(user=profile.user)
+                & ~Q(id__in=follows)
+                & Q(tags__in=profile.tags.all())
+                & ~Q(privacity="N")
+                & Q(user__is_active=True)
+            )
+            .annotate(similarity=Count("tags"))
+            .order_by("-similarity")
+            .select_related("user")
+        )
 
         if not users:
             continue
 
         mail = Mailer()
-        mail.send_messages('Skyfolk - Tenemos recomendaciones para ti.', template='emails/recommendations.html',
-                           context={'to_user': profile.user.username, 'object_list': users},
-                           to_emails=[profile.user.email])
+        mail.send_messages(
+            "Skyfolk - Tenemos recomendaciones para ti.",
+            template="emails/recommendations.html",
+            context={"to_user": profile.user.username, "object_list": users},
+            to_emails=[profile.user.email],
+        )
